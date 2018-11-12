393fb8ec2ea3a2a185fc671116b89b04769fe8a9
user <mhmadabas17@gmail.com>
2018-11-12 13:45:34 +0200

    url button

--- app/src/main/java/com/example/user/partytime/Ijump.java
+++ app/src/main/java/com/example/user/partytime/Ijump.java
@@ -1,13 +1,47 @@
 package com.example.user.partytime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Ijump extends AppCompatActivity {
+public class Ijump extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvIjump, textView5;
+
+    ImageView imageView3;
+
+    Button btUrl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ijump);
+
+        tvIjump= findViewById(R.id.tvIjump);
+        textView5 = findViewById(R.id.textView5);
+
+        imageView3 = findViewById(R.id.imageView3);
+
+        btUrl = findViewById(R.id.btUrl);
+        btUrl.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btUrl){
+            String url = "http://www.ijump.co.il/";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/user/partytime/Xpark.java
+++ app/src/main/java/com/example/user/partytime/Xpark.java
@@ -1,13 +1,43 @@
 package com.example.user.partytime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Xpark extends AppCompatActivity {
+public class Xpark extends AppCompatActivity implements View.OnClickListener {
+    TextView tvXpark,textView4;
+
+    ImageView imageView4;
+
+    Button btUrl2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xpark);
+
+        tvXpark = findViewById(R.id.tvXpark);
+        textView4 = findViewById(R.id.textView4);
+
+        imageView4 = findViewById(R.id.imageView4);
+
+        btUrl2 = findViewById(R.id.btUrl2);
+        btUrl2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btUrl2){
+            String url = "https://www.pex.co.il/";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
+
     }
 }
