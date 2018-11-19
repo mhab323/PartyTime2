a78d8ce4e4a843496031a565f734b609dedf792e
user <mhmadabas17@gmail.com>
2018-11-19 13:14:24 +0200

    birthdat custom adapter

--- app/src/main/java/com/example/user/partytime/BirthdayCustomAdapter.java
+++ app/src/main/java/com/example/user/partytime/BirthdayCustomAdapter.java
@@ -1,60 +1,57 @@
-//package com.example.user.partytime;
-//
-//import android.content.Context;
-//import android.graphics.Bitmap;
-//import android.graphics.BitmapFactory;
-//import android.support.annotation.NonNull;
-//import android.util.Base64;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import java.util.ArrayList;
-//
-//public class BirthdayCustomAdapter extends ArrayAdapter<Item> {
-//
-//    private int resourceLayout;
-//    private Context mContext;
-//
-//
-//   // public BirthdayCustomAdapter(@NonNull View.OnClickListener context, int resource, @NonNull ArrayList<BirthDay> objects) {
-//      //  super((Context) context, resource, objects);
-//
-//        this.resourceLayout=resource;
-//        this.mContext= (Context) context;
-//}
-//
-//
-////
-////    @Override
-////    public View getView(int position, View convertView, ViewGroup parent){
-////        View v= convertView;
-////        if(v==null)
-////            v= LayoutInflater.from(mContext).inflate(resourceLayout,parent,false);
-////
-////      //  BirthDay p= getBirthDay(position);
-////
-////       // if (p != null){
-////
-////            TextView tvName = (TextView) v.findViewById(R.id.tvTitle1);
-////            tvName.setText(p.getDate());
-////
-////
-////        }
-////         return v;
-////    }
-//    public Bitmap StringToBitMap(String encodedString) {
-//        try {
-//            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
-//            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0,
-//                    encodeByte.length);
-//            return bitmap;
-//        } catch (Exception e) {
-//            e.getMessage();
-//            return null;
-//        }
-//    }
-//}
+package com.example.user.partytime;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BirthdayCustomAdapter extends ArrayAdapter<BirthDay> {
+
+    private int resourceLayout;
+    private Context mContext;
+
+
+    public BirthdayCustomAdapter(@NonNull View.OnClickListener context, int resource, @NonNull ArrayList<BirthDay> objects) {
+        super((Context) context, resource, objects);
+
+        this.resourceLayout=resource;
+        this.mContext= (Context) context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View v= convertView;
+        if(v==null)
+            v= LayoutInflater.from(mContext).inflate(resourceLayout,parent,false);
+
+        BirthDay p= getItem(position);
+
+        if (p != null) {
+
+            TextView tvName = (TextView) v.findViewById(R.id.tvTitle1);
+            tvName.setText(p.getDate());
+        }
+        return v;
+
+    }
+
+    public Bitmap StringToBitMap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0,
+                    encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/example/user/partytime/addParty.java
+++ app/src/main/java/com/example/user/partytime/addParty.java
@@ -1,49 +1,49 @@
-//package com.example.user.partytime;
-//
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ImageButton;
-//import android.widget.ListView;
-//import android.widget.TextView;
-//
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//
-//import java.util.ArrayList;
-//
-//public class addParty extends AppCompatActivity implements View.OnClickListener{
-//    TextView tvAddParty;
-//
-//     ListView lvParties;
-//
-//    ImageButton addItem2;
-//
-//    ArrayList<BirthDay> Birthday2;
-//    CustomAdapter adapter2;
-//
-//    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    final DatabaseReference myRef = database.getReference().child("Birthdays");
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_add_party);
-//
-//        tvAddParty = findViewById(R.id.tvAddParty);
-//
-//        addItem2 = findViewById(R.id.addItem);
-//        addItem2.setOnClickListener(this);
-//
-//        Birthday2= new ArrayList<>();
-//
-//        lvParties = findViewById(R.id.LvBdList);
-//        adapter2 = new BirthdayCustomAdapter(this,R.layout.custom_row,Birthday2);
-//        lvParties.setAdapter(adapter2);
-//    }
-//
-//    @Override
-//    public void onClick(View v) {
-//
-//    }
-//}
+package com.example.user.partytime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class addParty extends AppCompatActivity implements View.OnClickListener{
+    TextView tvAddParty;
+
+     ListView lvParties;
+
+    ImageButton addItem2;
+
+    ArrayList<BirthDay> Birthday2;
+    BirthdayCustomAdapter adapter2;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef = database.getReference().child("Birthdays");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_party);
+
+        tvAddParty = findViewById(R.id.tvAddParty);
+
+        addItem2 = findViewById(R.id.addItem);
+        addItem2.setOnClickListener(this);
+
+        Birthday2= new ArrayList<>();
+
+        lvParties = findViewById(R.id.LvBdList);
+        adapter2 = new BirthdayCustomAdapter(this,R.layout.custom_row,Birthday2);
+        lvParties.setAdapter(adapter2);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
