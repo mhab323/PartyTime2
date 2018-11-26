3f90d4eb0e9a06620b9b3f532010f0d8fec9933a
user <mhmadabas17@gmail.com>
2018-11-26 13:41:07 +0200

    save image

--- app/src/main/java/com/example/user/partytime/NewBDActivity.java
+++ app/src/main/java/com/example/user/partytime/NewBDActivity.java
@@ -2,10 +2,12 @@ package com.example.user.partytime;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,8 +24,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NewBDActivity extends AppCompatActivity implements View.OnClickListener {
     Bitmap photo;
@@ -64,7 +71,6 @@ public class NewBDActivity extends AppCompatActivity implements View.OnClickList
         imageView = findViewById(R.id.imageView2);
 
 
-
     }
 
     @Override
@@ -77,11 +83,11 @@ public class NewBDActivity extends AppCompatActivity implements View.OnClickList
                 i.putExtra("item", item);
                 myRef.push().setValue(item);
 
-              //  Toast.makeText(getApplicationContext(), "Button is clicked", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Button is clicked", Toast.LENGTH_LONG).show();
                 startActivity(i);
                 break;
-                default:
-                    break;
+            default:
+                break;
 
         }
 
@@ -101,6 +107,16 @@ public class NewBDActivity extends AppCompatActivity implements View.OnClickList
 
             photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+          String imagePath= saveImage(photo);
+
+            SharedPreferences pref = getSharedPreferences("myPref",MODE_PRIVATE);
+            //open the file for editing
+            SharedPreferences.Editor editor = pref.edit();
+            //Keyword , value format of values to be saved in the sharedprefrences
+            //can be several values
+            editor.putString("image",imagePath);
+
+            editor.commit();
         } else if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
             Uri targetUri = data.getData();
             try {
@@ -111,6 +127,7 @@ public class NewBDActivity extends AppCompatActivity implements View.OnClickList
 
         }
     }
+
     public String BitMapToString(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
@@ -118,6 +135,33 @@ public class NewBDActivity extends AppCompatActivity implements View.OnClickList
         String temp = Base64.encodeToString(b, Base64.DEFAULT);
         return temp;
     }
+
+
+    public String saveImage(Bitmap bitmap) {
+        File root = Environment.getExternalStorageDirectory();
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        String filePath = root.getAbsolutePath() + "/DCIM/Camera/IMG" + timeStamp + ".jpg";
+        //creating an object from type file
+        File file = new File(filePath);
+
+        try {
+            file.createNewFile();
+            FileOutputStream ostream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+            ostream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Faild to save image", Toast.LENGTH_SHORT).show();
+
+        }
+        return filePath;
+    }
+
+
+
 }
 
 
