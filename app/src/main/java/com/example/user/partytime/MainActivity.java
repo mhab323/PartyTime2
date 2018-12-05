651d1183bfb386e135202ba280a91afe98ffb431
mhmad abbas <mhmadabas17@gmail.com>
2018-12-05 14:22:41 +0200

    dfghxdfgxdfg

--- app/src/main/java/com/example/user/partytime/MainActivity.java
+++ app/src/main/java/com/example/user/partytime/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.partytime;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Movie;
@@ -11,19 +12,42 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.InputStream;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tvPartyTime;
+
+    Button btStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvPartyTime = findViewById(R.id.tvPartyTime);
+
+        btStart = findViewById(R.id.btStart);
+        btStart.setOnClickListener(this);
+
+
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btStart: {
+                Intent tent = new Intent(this, LogInorSignup.class);
+                Toast.makeText(getApplicationContext(), "Button is clicked", Toast.LENGTH_LONG).show();
+                startActivity(tent);
+
+            }
+        }
+    }
 }
